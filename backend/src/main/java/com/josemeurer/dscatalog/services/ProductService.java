56c49b69405aebf3ceeb0304a2e7ba168ffde403
JoseMeurer <josewm18@gmail.com>
2022-08-12 14:47:59 -0300

    Product insert, update, delete

--- backend/src/main/java/com/josemeurer/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/josemeurer/dscatalog/services/ProductService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.josemeurer.dscatalog.dto.ProductDTO;
 import com.josemeurer.dscatalog.entities.Product;
+import com.josemeurer.dscatalog.repositories.CategoryRepository;
 import com.josemeurer.dscatalog.repositories.ProductRepository;
 import com.josemeurer.dscatalog.services.exceptions.DatabaseException;
 import com.josemeurer.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +24,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository catRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -40,18 +44,18 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
-		return new ProductDTO(entity);
+		return new ProductDTO(entity, entity.getCategories());
 	}
 
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) { 
 		try {
 			Product entity = repository.getOne(id);
-			//entity.setName(dto.getName());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
-			return new ProductDTO(entity);
+			return new ProductDTO(entity, entity.getCategories());
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("id not found " + id);
@@ -68,5 +72,16 @@ public class ProductService {
 		catch (DataIntegrityViolationException e) { 
 			throw new DatabaseException("Integrity violation");
 		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+
+		entity.getCategories().clear();
+		dto.getCategories().forEach(x -> entity.getCategories() //Fiz de outro jeito;
+				.add(catRepository.getOne(x.getId()))); //getOne deprecated, o certo é usar getById;
 	}
 }
