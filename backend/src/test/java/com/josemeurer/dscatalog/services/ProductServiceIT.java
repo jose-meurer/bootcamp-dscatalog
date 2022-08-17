d90e3573c242cbeb4cef9940c35e7b287603f4df
JoseMeurer <josewm18@gmail.com>
2022-08-16 21:12:01 -0300

    ProductServiceIT findAllPaged test

--- backend/src/test/java/com/josemeurer/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/josemeurer/dscatalog/services/ProductServiceIT.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.josemeurer.dscatalog.dto.ProductDTO;
 import com.josemeurer.dscatalog.repositories.ProductRepository;
 import com.josemeurer.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional //Faz rollback no banco a cada teste;
 public class ProductServiceIT {
 
 	@Autowired
@@ -30,6 +36,42 @@ public class ProductServiceIT {
 		nonExistingId = 1000L;
 		countTotalProducts = repository.count();
 		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("price"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertTrue(result.getContent().get(0).getPrice() < result.getContent().get(1).getPrice());
+		Assertions.assertTrue(result.getContent().get(2).getPrice() < result.getContent().get(3).getPrice());
+		Assertions.assertTrue(result.getContent().get(4).getPrice() < result.getContent().get(5).getPrice());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPageArguments() { //Page 0, Size 10;
+		
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertTrue(result.getSize() == 10);
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
 	}
 	
 	@Test
