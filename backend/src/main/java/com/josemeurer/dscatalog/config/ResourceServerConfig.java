1ab37680857f5eb69a4d023633856b46b911b356
JoseMeurer <josewm18@gmail.com>
2022-08-29 20:53:50 -0300

    CHORE H2 security configuration

--- backend/src/main/java/com/josemeurer/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/josemeurer/dscatalog/config/ResourceServerConfig.java
@@ -2,12 +2,15 @@ package com.josemeurer.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer //Esta classe implementa o resource server do Oauth2
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token" };
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**" };
 
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**" };
 
@@ -30,6 +36,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Se o app.properties rodando for de teste, ele libera o frame do h2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll() //Define as autorizaçoes
