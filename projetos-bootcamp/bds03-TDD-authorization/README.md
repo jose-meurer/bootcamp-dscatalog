81fe2baa9898463a1a53f34cdb7ea46968a13c34
joseMeurer <josewmeurer@gmail.com>
2023-09-30 18:17:56 -0300

    repo bds03-TDD-authorization

---
+++ projetos-bootcamp/bds03-TDD-authorization/README.md
@@ -0,0 +1,14 @@
+## Project TDD validation and security
+
+> ### <h2> Project Tasks </h2>
+
+![image](https://user-images.githubusercontent.com/75624436/187694770-2ae27c80-ec33-4e54-a379-f523c80ced0c.png)
+
+> ### <h2> Spring Security </h2>
+
+![image](https://user-images.githubusercontent.com/75624436/187718574-936bf9d5-aa54-40cb-933f-2b55bc051256.png)
+
+> ### <h2> Spring Cloud OAuth2 </h2>
+
+![image](https://user-images.githubusercontent.com/75624436/187718667-12a6adae-acde-458a-8cb5-f7b0f36e1e4a.png)
+
---
+++ projetos-bootcamp/bds03-TDD-authorization/src/main/java/com/devsuperior/bds03/entities/User.java
@@ -0,0 +1,115 @@
+package com.devsuperior.bds03.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable, UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getAuthority())).toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !roles.isEmpty();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !roles.isEmpty();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !roles.isEmpty();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
